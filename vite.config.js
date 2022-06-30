3c35e685c90dc0b6e0fbc71b87f4ae8345f38ae2
none <none@none.com>
2022-06-29 20:50:27 -0400

    chore: remove unnecessary config

--- vite.config.js
+++ vite.config.js
@@ -6,22 +6,6 @@ export default defineConfig({
   build: {
     target: 'es6'
   },
-  css: {
-    postcss: {
-      plugins: [
-        require('postcss-preset-env')({
-          browsers: [
-            'Chrome >= 61',
-            'Safari >= 10.1',
-            "iOS >= 10.3",
-            "Firefox >= 60",
-            "Edge >= 16",
-            "Opera >= 48"
-          ]
-        })
-      ]
-    }
-  },
   plugins: [
     vue()
   ]
